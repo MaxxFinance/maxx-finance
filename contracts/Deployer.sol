816f26216e5bcacd869015151f98cdd91544ab21
Ammon Werner <ammon@altafin.co>
2022-06-18 09:53:19 -0600

    Add NatSpec

--- contracts/Deployer.sol
+++ contracts/Deployer.sol
@@ -2,8 +2,12 @@
 pragma solidity ^0.8.14;
 
 contract Deployer {
+    /// @notice Emitted when a contract is deployed
     event Deployed(address addr, uint256 salt);
 
+    /// @notice Deploys a contract using the create2 method
+    /// @param code The bytecode of the contract to deploy
+    /// @param salt The salt to use for the create2 method
     function deploy(bytes memory code, uint256 salt) public {
         address addr;
         assembly {
