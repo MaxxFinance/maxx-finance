bfe0c6488d69a6185bb38b3921bc5bc4ddd148b4
Ammon Werner <ammon@altafin.co>
2022-06-28 03:32:50 -0600

    Create Marketplace.sol

---
+++ contracts/Marketplace.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+import { MaxxStake as Stake } from './MaxxStake.sol';
+
+/// @author Alta Web3 Labs
+contract Marketplace is Ownable {
+    
+}
