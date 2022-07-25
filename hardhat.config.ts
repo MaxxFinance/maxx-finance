d0f4789087b40c3f163e0aabca8186c0b21163ea
Ammon Werner <ammon@altafin.co>
2022-07-25 08:17:26 -0600

    Update to hardhat-toolbox

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,26 +1,11 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-chai-matchers";
+import "@nomiclabs/hardhat-solhint";
+import "hardhat-docgen";
+
 import * as dotenv from "dotenv";
-
-import { HardhatUserConfig, task } from "hardhat/config";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
-
 dotenv.config();
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -58,6 +43,11 @@ const config: HardhatUserConfig = {
       polygon: process.env.POLYGONSCAN_API_KEY!,
       polygonMumbai: process.env.POLYGONSCAN_API_KEY!,
     },
+  },
+  docgen: {
+    path: "./docs",
+    clear: true,
+    runOnCompile: true,
   },
 };
 
