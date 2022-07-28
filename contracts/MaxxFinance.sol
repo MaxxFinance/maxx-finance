f458b2d99e4e9020622e26ec383ea11f933ed7e0
Ammon Werner <ammon@altafin.co>
2022-07-27 21:32:42 -0600

    Remove unused _burn override

--- contracts/MaxxFinance.sol
+++ contracts/MaxxFinance.sol
@@ -307,22 +307,4 @@ contract MaxxFinance is ERC20, ERC20Burnable, AccessControl, Pausable {
 
         super._beforeTokenTransfer(_from, _to, _amount);
     }
-
-    // function _burn(address account, uint256 amount) internal overrides {
-    //     require(account != address(0), "ERC20: burn from the zero address");
-
-    //     _beforeTokenTransfer(account, address(0), amount);
-
-    //     uint256 accountBalance = _balances[account];
-    //     require(accountBalance >= amount, "ERC20: burn amount exceeds balance");
-    //     unchecked {
-    //         _balances[account] = accountBalance - amount;
-    //         // Overflow not possible: amount <= accountBalance <= totalSupply.
-    //         _totalSupply -= amount;
-    //     }
-
-    //     emit Transfer(account, address(0), amount);
-
-    //     _afterTokenTransfer(account, address(0), amount);
-    // }
 }
