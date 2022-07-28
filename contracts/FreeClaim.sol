7e0bebed3ec2b56fa3283b54e82dc785263b0969
Ammon Werner <ammon@altafin.co>
2022-07-27 19:16:47 -0600

    Update for automatic claim staking

--- contracts/FreeClaim.sol
+++ contracts/FreeClaim.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.15;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -20,14 +20,51 @@ error FreeClaimEnded();
 /// User cannot refer themselves
 error SelfReferral();
 
-// /// The Maxx Finance Staking contract hasn't been initialized
-// error StakingNotInitialized();
+/// The Maxx Finance Staking contract hasn't been initialized
+error StakingNotInitialized();
 
-// TODO: discuss staking contract initialization
+/// Only the Staking contract can call this function
+error OnlyMaxxStake();
+
 /// @title Maxx Finance Free Claim
 /// @author Alta Web3 Labs - SonOfMosiah
 contract FreeClaim is Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
+
+    /// Merkle root for the free claim whitelist
+    bytes32 public merkleRoot; // TODO: may need to create multiple merkle roots depending on Merkle Tree size.
+
+    /// Free claim start date
+    uint256 public immutable startDate;
+    uint256 constant FREE_CLAIM_DURATION = 365 days;
+
+    /// Max number of MAXX tokens that can be claimed by a user
+    uint256 public constant MAX_CLAIM_AMOUNT = 1000000 * (10**8); // 1 million MAXX
+
+    /// MAXX token contract
+    IERC20 public MAXX;
+
+    /// MAXX staking contract
+    IStake public maxxStake;
+
+    /// True if user has already claimed MAXX
+    mapping(address => bool) public hasClaimed;
+
+    /// Mapping claims to owners
+    mapping(address => uint256) public claimOwners;
+    /// Array of staked claims
+    Claim[] private stakedClaims;
+    /// Array of unstaked claims;
+    Claim[] private unstakedClaims;
+
+    uint256 private claimedAmount;
+    uint256 private remainingBalance;
+    uint256 private maxxAllocation;
+
+    struct Claim {
+        address user;
+        uint256 amount;
+    }
 
     /// @notice Emitted when free claim is claimed
     /// @param user The user claiming free claim
@@ -44,36 +81,6 @@ contract FreeClaim is Ownable, ReentrancyGuard {
         uint256 amount
     );
 
-    // /// @notice Emitted when claim is staked
-    // /// @param user The user staking the claim
-    // /// @param amount The amount of claim staked
-    // event StakeClaim(address indexed user, uint256 amount);
-
-    // TODO: may need to create multiple merkle roots depending on Merkle Tree size.
-    /// Merkle root for the free claim whitelist
-    bytes32 public merkleRoot;
-
-    /// Free claim start date
-    uint256 public immutable startDate;
-    uint256 constant FREE_CLAIM_DURATION = 365 days;
-
-    /// Max number of MAXX tokens that can be claimed by a user
-    uint256 public constant MAX_CLAIM_AMOUNT = 1000000 * (10**8); // 1 million MAXX
-
-    /// Maxx Finance staking contract
-    IStake public stake;
-
-    /// MAXX token contract
-    IERC20 public MAXX;
-
-    /// True if user has already claimed MAXX
-    mapping(address => bool) public hasClaimed;
-    mapping(address => bool) public hasStaked;
-
-    /// Mapping users to their claim amounts
-    mapping(address => uint256) public claims;
-    uint256 private claimedAmount;
-
     constructor(
         uint256 _startDate,
         bytes32 _merkleRoot,
@@ -82,11 +89,6 @@ contract FreeClaim is Ownable, ReentrancyGuard {
         startDate = _startDate;
         merkleRoot = _merkleRoot;
         MAXX = IERC20(_MAXX);
-    }
-
-    struct Claim {
-        address user;
-        uint256 amount;
     }
 
     /// @notice Function to retrive free claim
@@ -109,8 +111,7 @@ contract FreeClaim is Ownable, ReentrancyGuard {
             revert InvalidProof();
         }
 
-        uint256 contractBalance = MAXX.balanceOf(address(this));
-        if (contractBalance == 0) {
+        if (remainingBalance == 0) {
             revert FreeClaimEnded();
         }
 
@@ -124,39 +125,89 @@ contract FreeClaim is Ownable, ReentrancyGuard {
             (_amount * (FREE_CLAIM_DURATION - timePassed)) /
             FREE_CLAIM_DURATION; // adjust amount for the speed penalty
 
-        if (_amount > contractBalance) {
-            // No referral bonus if contract balance is less than the amount to claim
-            _amount = contractBalance;
+        if (
+            address(maxxStake) != address(0) &&
+            maxxStake.launchDate() < block.timestamp
+        ) {
+            if (_amount > remainingBalance) {
+                // No referral bonus if contract balance is less than the amount to claim
+                _amount = remainingBalance;
 
-            claims[msg.sender] += _amount;
-            claimedAmount += _amount;
-            emit UserClaim(msg.sender, _amount);
-        } else {
-            if (_referrer != address(0)) {
-                uint256 referralAmount = _amount / 10;
-                _amount += referralAmount; // +10% bonus for referral
-                claims[_referrer] += referralAmount;
-                claimedAmount += referralAmount;
-                emit UserClaim(_referrer, referralAmount);
-                emit Referral(_referrer, msg.sender, referralAmount);
+                stakedClaims.push(Claim(msg.sender, _amount));
+                claimedAmount += _amount;
+                emit UserClaim(msg.sender, _amount);
+            } else {
+                if (_referrer != address(0)) {
+                    uint256 referralAmount = _amount / 10;
+                    _amount += referralAmount; // +10% bonus for referral
+                    stakedClaims.push(Claim(_referrer, referralAmount));
+                    claimedAmount += referralAmount;
+                    emit UserClaim(_referrer, referralAmount);
+                    emit Referral(_referrer, msg.sender, referralAmount);
+                }
+
+                hasClaimed[msg.sender] = true;
+
+                stakedClaims.push(Claim(msg.sender, _amount));
+                claimedAmount += _amount;
+                emit UserClaim(msg.sender, _amount);
             }
+        } else {
+            if (_amount > remainingBalance) {
+                // No referral bonus if contract balance is less than the amount to claim
+                _amount = remainingBalance;
 
-            hasClaimed[msg.sender] = true;
+                unstakedClaims.push(Claim(msg.sender, _amount));
+                claimedAmount += _amount;
+                emit UserClaim(msg.sender, _amount);
+            } else {
+                if (_referrer != address(0)) {
+                    uint256 referralAmount = _amount / 10;
+                    _amount += referralAmount; // +10% bonus for referral
+                    unstakedClaims.push(Claim(_referrer, referralAmount));
+                    claimedAmount += referralAmount;
+                    emit UserClaim(_referrer, referralAmount);
+                    emit Referral(_referrer, msg.sender, referralAmount);
+                }
 
-            claims[msg.sender] += _amount;
-            claimedAmount += _amount;
-            emit UserClaim(msg.sender, _amount);
+                hasClaimed[msg.sender] = true;
+
+                stakedClaims.push(Claim(msg.sender, _amount));
+                claimedAmount += _amount;
+                emit UserClaim(msg.sender, _amount);
+            }
         }
     }
 
-    /// @notice Function to stake free claim
-    function stakeClaim() external {
-        if (address(stake == address(0))) {
-            revert StakingNotInitialized();
+    /// @notice Add MAXX to the free claim allocation
+    /// @param _amount The amount of MAXX to add to the free claim allocation
+    function allocateMaxx(uint256 _amount) external onlyOwner {
+        MAXX.transferFrom(msg.sender, address(this), _amount);
+        maxxAllocation += _amount;
+    }
+
+    /// @notice Set the Maxx Finance Staking contract
+    /// @param _maxxStake The Maxx Finance Staking contract
+    function setMaxxStake(address _maxxStake) external onlyOwner {
+        maxxStake = IStake(_maxxStake);
+    }
+
+    /// @notice Update the merkle root
+    /// @param _merkleRoot new merkle root
+    function updateMerkleRoot(bytes32 _merkleRoot) external onlyOwner {
+        merkleRoot = _merkleRoot;
+    }
+
+    /// @notice Stake the unstaked claims
+    function stakeClaims() external {
+        if (msg.sender != address(maxxStake) || msg.sender == address(0)) {
+            revert OnlyMaxxStake();
         }
-        uint256 claimAmount = claims[msg.sender].amount; // retrieve user's claim from storage
-        stake.freeClaimStake(msg.sender, claimAmount);
-        emit StakeClaim(msg.sender, claimAmount);
+
+        for (uint256 i = 0; i < unstakedClaims.length; i++) {
+            stakedClaims.push(unstakedClaims[i]);
+        }
+        delete unstakedClaims;
     }
 
     /// @param _account The account presumed to be in the merkle tree
@@ -174,18 +225,6 @@ contract FreeClaim is Ownable, ReentrancyGuard {
                 merkleRoot,
                 _generateMerkleLeaf(_account, _amount)
             );
-    }
-
-    /// @notice Update the merkle root
-    /// @param _merkleRoot new merkle root
-    function updateMerkleRoot(bytes32 _merkleRoot) external onlyOwner {
-        merkleRoot = _merkleRoot;
-    }
-
-    /// @notice Function to set the Maxx Finance staking contract address
-    /// @param _stake Address of the Maxx Finance staking contract
-    function setStakeAddress(address _stake) external onlyOwner {
-        stake = IStake(_stake);
     }
 
     function _generateMerkleLeaf(address _account, uint256 _amount)
