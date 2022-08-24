cb18729818d4867d27be1137e8a1cf4acd4dfef2
Ammon Werner <ammon@altafin.co>
2022-08-24 15:27:21 -0600

    Update Free Claim deployer script

--- scripts/deploy/FreeClaim.ts
+++ scripts/deploy/FreeClaim.ts
@@ -1,67 +1,28 @@
-import hre, { ethers } from "hardhat";
-import {
-  FreeClaim,
-  FreeClaim__factory,
-  MaxxFinance,
-  MaxxFinance__factory,
-  MaxxStake,
-  MaxxStake__factory,
-} from "../../typechain-types";
-import log from "ololog";
+import hre, { ethers } from 'hardhat';
+import { FreeClaim__factory } from '../../typechain-types';
+import log from 'ololog';
 
 async function main() {
-  const maxxVaultAddress = "";
-  const transferTax = "500"; // 5%
-  const whaleLimit = "1000000"; // 1 million
-  const globalSellLimit = "1000000000"; // 1 billion
-  const claimLaunchDate = "";
-  const merkleRoot = "";
-  const amplifierLaunchDate = "";
-  const stakeLaunchDate = "";
-  const maxxBoostAddress = "";
-  const maxxGenesisAddress = "";
+    const claimLaunchDate = '1661407200'; // Thu Aug 25 2022 06:00:00 GMT+0000
+    const maxxFinance = {
+        address: process.env.MAXX_FINANCE_ADDRESS!,
+    };
 
-  const FreeClaim = (await ethers.getContractFactory(
-    "FreeClaim"
-  )) as FreeClaim__factory;
-  const MaxxFinance = (await ethers.getContractFactory(
-    "MaxxFinance"
-  )) as MaxxFinance__factory;
-  const MaxxStake = (await ethers.getContractFactory(
-    "MaxxStake"
-  )) as MaxxStake__factory;
+    const FreeClaim = (await ethers.getContractFactory(
+        'FreeClaim'
+    )) as FreeClaim__factory;
 
-  const freeClaim = await FreeClaim.deploy(
-    claimLaunchDate,
-    merkleRoot,
-    maxxFinance.address
-  );
+    const freeClaim = await FreeClaim.deploy(
+        claimLaunchDate,
+        maxxFinance.address
+    );
 
-  log.yellow("freeClaim.address: ", freeClaim.address);
-
-  const liquidityAmplifier = await LiquidityAmplifier.deploy(
-    maxxVaultAddress,
-    amplifierLaunchDate,
-    maxxFinance.address
-  );
-  log.yellow("liquidityAmplifier.address: ", liquidityAmplifier.address);
-
-  const maxxStake = await MaxxStake.deploy(
-    maxxVaultAddress,
-    maxxFinance.address,
-    stakeLaunchDate,
-    maxxBoostAddress,
-    maxxGenesisAddress
-  );
-  log.yellow("maxxStake.address: ", maxxStake.address);
-
-  const marketplace = await Marketplace.deploy(maxxStake.address);
-  log.yellow("marketplace.address: ", marketplace.address);
+    log.yellow('freeClaim.address: ', freeClaim.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
